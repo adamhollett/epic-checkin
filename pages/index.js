541a72c3ff154ab7116b291e43a840444694025f
Adam Hollett <mail@adamhollett.com>
2020-01-21 23:05:00 -0500

    Add basic GraphQL API

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
-function Index() {
-  return <div>Hello world!</div>
+import axios from "axios"
+
+const Index = props => {
+  return <div>The API responded: { props.hello }</div>
+}
+
+Index.getInitialProps = async () => {
+  const query = "{ hello }"
+  const response = await axios.post("http://localhost:3000/api/graphql", {
+    query
+  })
+
+  return { ...response.data.data }
 }
 
 export default Index
